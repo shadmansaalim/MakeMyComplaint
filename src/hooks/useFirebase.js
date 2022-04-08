476638aa8c3766def0eb1e64cc2bef6bca36a43b
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-04-08 23:50:37 +1000

    Firebase added, custom hook built for authentication, sign up function code done

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,64 @@
+import initializeAuthentication from "../Firebase/firebase.init";
+import { getAuth, createUserWithEmailAndPassword, updateProfile, onAuthStateChanged, getIdToken } from "firebase/auth";
+import { useState } from "react";
+import { useEffect } from "react";
+
+initializeAuthentication();
+
+
+const useFirebase = () => {
+    const [user, setUser] = useState({})
+    const [isLoading, setIsLoading] = useState(true);
+
+    const auth = getAuth();
+
+    const registerUser = (name, email, password, history) => {
+        setIsLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const newUser = { email, displayName: name };
+                setUser(newUser);
+
+
+                // Send name to firebase
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                }).then(() => {
+                    history.replace('/');
+                }).catch((error) => {
+
+                });
+
+            })
+            .catch((error) => {
+                // ..
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            setIsLoading(true);
+            setUser(user);
+            getIdToken(user)
+                .then(token => {
+                    localStorage.setItem('token', token)
+
+                })
+
+            setIsLoading(false);
+
+
+        });
+    }, [auth, user.email])
+
+
+    return {
+        user,
+        isLoading,
+        registerUser,
+    }
+};
+
+export default useFirebase;
